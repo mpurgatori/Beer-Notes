139cbd29e434ba3a4e620d308dfba1ee0f852b0b
Mike Purgatori <mpurgatori@gmail.com>
2017-11-13 10:10:07 -0600

    vue component mounting

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import App from './app.vue'
+import logIn from './components/log-in.vue';
 
 Vue.use(VueRouter);
 
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 
+const routes = [
+  {
+        name: 'logIn',
+        path: '/',
+        component: logIn
+    }
+];
+
+
 Vue.use(VueAxios, axios);
 
-const router = new VueRouter({ mode: 'history' });
-new Vue(Vue.util.extend({ router })).$mount('#app');
+const router = new VueRouter({ mode: 'history', routes: routes });
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
